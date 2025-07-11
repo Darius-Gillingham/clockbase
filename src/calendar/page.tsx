e9454d2b39d2018f904c54299b873507ef318b43
Darius.Gillingham <darius.gillingham@student.ufv.ca>
2025-07-10 23:16:30 -0700

    calendar 4

--- src/calendar/page.tsx
+++ src/calendar/page.tsx
@@ -1,11 +1,10 @@
+'use client'
+
 // File: src/app/calendar/page.tsx
 // Commit: restore working Calendar route
 
+import CalendarView from './CalendarView'
+
 export default function CalendarPage() {
-  return (
-    <div className="p-6 text-black">
-      <h1 className="text-2xl font-bold mb-4">Team Calendar</h1>
-      <p>Calendar functionality will be here.</p>
-    </div>
-  )
+  return <CalendarView />
 }
